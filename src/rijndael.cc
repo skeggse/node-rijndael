feb8456619e1b4d928eb2a5cc90ec1fbbecafe9b
Eli Skeggs <eskeggs@globesherpa.com>
2013-07-18 11:55:32 -0700

    [0.0.2] add decrypt method, add Rijndael wrapper class
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- src/rijndael.cc
+++ src/rijndael.cc
@@ -10,91 +10,79 @@
 using namespace v8;
 using namespace node;
 
-// rijndael.encrypt(plaintext, key)
-Handle<Value> Encrypt(const Arguments& args) {
+Handle<Value> Rijndael(const Arguments& args) {
   HandleScope scope;
 
-  if (args.Length() < 1 || !Buffer::HasInstance(args[0])) {
-    ThrowException(Exception::TypeError(String::New("plaintext must be a buffer")));
-    return scope.Close(Undefined());
-  }
+  MCRYPT rijndael_module;
 
-  if (args.Length() < 2 || !Buffer::HasInstance(args[1])) {
-    ThrowException(Exception::TypeError(String::New("key must be a buffer")));
-    return scope.Close(Undefined());
-  }
-
-  int err;
+  int err = 0;
+  char* error_message;
   int data_size;
   void* data;
-  char* plaintext;
+  char* text;
   char* key;
+  int text_len;
+  int key_len;
+  bool encrypt;
 
-  plaintext = Buffer::Data(args[0]);
-  key = Buffer::Data(args[1]);
+  if (args.Length() < 1 || !Buffer::HasInstance(args[0])) {
+    err = 1; error_message = (char*) "data must be a buffer";
+  } else if (args.Length() < 2 || !Buffer::HasInstance(args[1])) {
+    err = 1; error_message = (char*) "key must be a buffer";
+  } else if (args.Length() < 3 || !args[2]->IsBoolean()) {
+    err = 1; error_message = (char*) "encryption must be a boolean";
+  }
 
-  int text_len = Buffer::Length(args[0]);
-  int key_len = Buffer::Length(args[1]);
-
-  MCRYPT rinjdael_module = mcrypt_module_open((char*) "rijndael-256", NULL, (char*) "ecb", NULL);
-
-  if (rinjdael_module == MCRYPT_FAILED) {
-    ThrowException(Exception::TypeError(String::New("loading rijndael mcrypt module failed")));
+  if (err == 1) {
+    ThrowException(Exception::TypeError(String::New(error_message)));
     return scope.Close(Undefined());
   }
+
+  text = Buffer::Data(args[0]);
+  key = Buffer::Data(args[1]);
+  encrypt = args[2]->BooleanValue();
+
+  text_len = Buffer::Length(args[0]);
+  key_len = Buffer::Length(args[1]);
 
   if (key_len != 32) {
     ThrowException(Exception::Error(String::New("key length does not match algorithm parameters")));
     return scope.Close(Undefined());
   }
 
+  rijndael_module = mcrypt_module_open((char*) "rijndael-256", NULL, (char*) "ecb", NULL);
+  if (rijndael_module == MCRYPT_FAILED) {
+    ThrowException(Exception::Error(String::New("rijndael mcrypt module failed to load")));
+    return scope.Close(Undefined());
+  }
+
   data_size = (((text_len - 1) / 32) + 1) * 32;
   data = malloc(data_size);
   memset(data, 0, data_size);
-  memcpy(data, plaintext, text_len);
+  memcpy(data, text, text_len);
 
-  err = mcrypt_generic_init(rinjdael_module, (void*) key, key_len, NULL);
+  err = mcrypt_generic_init(rijndael_module, (void*) key, key_len, NULL);
   if (err < 0) {
     ThrowException(Exception::Error(String::New(mcrypt_strerror(err))));
     return scope.Close(Undefined());
   }
 
-  mcrypt_generic(rinjdael_module, data, data_size);
+  if (encrypt)
+    mcrypt_generic(rijndael_module, data, data_size);
+  else
+    mdecrypt_generic(rijndael_module, data, data_size);
 
-  Buffer& encrypted = *Buffer::New((char*) data, data_size);
+  Buffer& buffer = *Buffer::New((char*) data, data_size);
 
-  mcrypt_module_close(rinjdael_module);
+  mcrypt_module_close(rijndael_module);
 
   free(data);
 
-  return scope.Close(encrypted.handle_);
+  return scope.Close(buffer.handle_);
 }
 
-/*Handle<Value> Decrypt(const Arguments& args) {
-  HandleScope scope;
-
-  if (!initialized) {
-    ThrowException(Exception::Error(String::New("not initialized")));
-    return scope.Close(Undefined());
-  }
-
-  if (args.Length() < 1 || !args[0]->IsString()) {
-    ThrowException(Exception::TypeError(String::New("plaintext must be a string")));
-    return scope.Close(Undefined());
-  }
-
-  int err;
-
-  String::AsciiValue plaintext(args[0]->ToString());
-
-  err = mcrypt_generic(rinjdael_module, plaintext, plaintext.length());
-
-  return scope.Close(String::New("decrypting..."));
-}*/
-
 void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("encrypt"), FunctionTemplate::New(Encrypt)->GetFunction());
-  //exports->Set(String::NewSymbol("decrypt"), FunctionTemplate::New(Decrypt)->GetFunction());
+  exports->Set(String::NewSymbol("rijndael"), FunctionTemplate::New(Rijndael)->GetFunction());
 }
 
 NODE_MODULE(rijndael, init)
