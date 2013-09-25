26cba5b0eac902036aa65fb9ee82ffdd57edfae5
Eli Skeggs <skeggse@gmail.com>
2013-09-25 11:49:13 -0700

    Add native support for iv, block mode
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- src/rijndael.cc
+++ src/rijndael.cc
@@ -21,9 +21,12 @@ Handle<Value> Rijndael(const Arguments& args) {
   void* data;
   char* text;
   char* key;
+  char* iv = NULL;
+
   int text_len;
   int key_len;
   bool encrypt;
+  char* mode;
 
   if (args.Length() < 1 || !Buffer::HasInstance(args[0])) {
     err = 1; error_message = (char*) "data must be a buffer";
@@ -31,6 +34,10 @@ Handle<Value> Rijndael(const Arguments& args) {
     err = 1; error_message = (char*) "key must be a buffer";
   } else if (args.Length() < 3 || !args[2]->IsBoolean()) {
     err = 1; error_message = (char*) "encryption must be a boolean";
+  } else if (args.Length() < 4) {
+    err = 1; error_message = (char*) "block mode must be a string";
+  } else if (args.Length() < 5) {
+    err = 1; error_message = (char*) "iv must be a buffer or null";
   }
 
   if (err == 1) {
@@ -38,9 +45,16 @@ Handle<Value> Rijndael(const Arguments& args) {
     return scope.Close(Undefined());
   }
 
+  if (Buffer::HasInstance(args[4])) {
+    iv = Buffer::Data(args[4]);
+  }
+
+  v8::String::Utf8Value modeStr(args[3]->ToString());
+
   text = Buffer::Data(args[0]);
   key = Buffer::Data(args[1]);
   encrypt = args[2]->BooleanValue();
+  mode = *modeStr;
 
   text_len = Buffer::Length(args[0]);
   key_len = Buffer::Length(args[1]);
@@ -50,14 +64,15 @@ Handle<Value> Rijndael(const Arguments& args) {
     return scope.Close(Undefined());
   }
 
-  rijndael_module = mcrypt_module_open((char*) "rijndael-256", NULL, (char*) "ecb", NULL);
+  rijndael_module = mcrypt_module_open((char*) "rijndael-256", NULL, mode, NULL);
   if (rijndael_module == MCRYPT_FAILED) {
     ThrowException(Exception::Error(String::New("rijndael mcrypt module failed to load")));
     return scope.Close(Undefined());
   }
 
-  err = mcrypt_generic_init(rijndael_module, (void*) key, key_len, NULL);
+  err = mcrypt_generic_init(rijndael_module, (void*) key, key_len, iv);
   if (err < 0) {
+    mcrypt_module_close(rijndael_module);
     ThrowException(Exception::Error(String::New(mcrypt_strerror(err))));
     return scope.Close(Undefined());
   }
@@ -68,16 +83,20 @@ Handle<Value> Rijndael(const Arguments& args) {
   memcpy(data, text, text_len);
 
   if (encrypt)
-    mcrypt_generic(rijndael_module, data, data_size);
+    err = mcrypt_generic(rijndael_module, data, data_size);
   else
-    mdecrypt_generic(rijndael_module, data, data_size);
+    err = mdecrypt_generic(rijndael_module, data, data_size);
+
+  if (err < 0) {
+    mcrypt_module_close(rijndael_module);
+    free(data);
+    ThrowException(Exception::Error(String::New(mcrypt_strerror(err))));
+    return scope.Close(Undefined());
+  }
 
   Buffer& buffer = *Buffer::New((char*) data, data_size);
-
   mcrypt_module_close(rijndael_module);
-
   free(data);
-
   return scope.Close(buffer.handle_);
 }
 
